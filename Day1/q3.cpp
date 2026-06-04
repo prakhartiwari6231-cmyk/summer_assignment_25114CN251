2c9cd58eefcd1f0535720b85192912b3b748d0f5
prakhartiwari6231-cmyk <prakhartiwari6231@gmail.com>
2026-06-05 01:06:40 +0530

    Refactor factorial calculation in q3.cpp
    Removed old factorial calculation and simplified the code.

--- Day1/q3.cpp
+++ Day1/q3.cpp
@@ -1,18 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+int main(){
+    int n,fact=1;
+ cout<<"enter num: ";
+cin>>n;
+for(int i=1;i<=n;i++)
+fact=fact*i;
+cout<<fact;
 
-int main() {
-    int n;
-    long long factorial = 1;
-
-    cout << "Enter a number: ";
-    cin >> n;
-
-    for(int i = 1; i <= n; i++) {
-        factorial = factorial * i;
-    }
-
-    cout << "Factorial of " << n << " = " << factorial;
-
-    return 0;
 }
