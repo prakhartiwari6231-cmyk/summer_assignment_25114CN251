e1c41ab2f7c558f9ee61c995dc4ea73bf5c909fc
prakhartiwari6231-cmyk <prakhartiwari6231@gmail.com>
2026-06-06 21:13:10 +0530

    completed assignments

--- Day3/q9.cpp
+++ Day3/q9.cpp
@@ -1,28 +1,38 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n, i;
+//     bool prime = true;
+
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+//     if (n <= 1) {
+//         prime = false;
+//     } else {
+//         for (i = 2; i <= n / 2; i++) {
+//             if (n % i == 0) {
+//                 prime = false;
+//                 break;
+//             }
+//         }
+//     }
+
+//     if (prime)
+//         cout << n << " is a Prime Number";
+//     else
+//         cout << n << " is not a Prime Number";
+
+//     return 0;
+// }
+
+#include<iostream>
 using namespace std;
-
-int main() {
-    int n, i;
-    bool prime = true;
-
-    cout << "Enter a number: ";
-    cin >> n;
-
-    if (n <= 1) {
-        prime = false;
-    } else {
-        for (i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
-                prime = false;
-                break;
-            }
-        }
-    }
-
-    if (prime)
-        cout << n << " is a Prime Number";
-    else
-        cout << n << " is not a Prime Number";
-
-    return 0;
+int main(){
+    int n;
+    cout<<"Entre a number: ";
+    cin>>n;
+    if(n%n==0 && n/1==n)
+    cout<<"prime number";
 }
